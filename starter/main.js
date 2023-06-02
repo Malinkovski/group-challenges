34be7e35b4d954dbb6a4754887f618b4ab4468b8
Ognen Bendevski <obendevski@gmail.com>
2023-06-02 19:17:56 +0200

    add changes

--- starter/main.js
+++ starter/main.js
@@ -1,38 +1,50 @@
-const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-const cakeImage = document.querySelector('.target');
+// const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+// const windowHeight =
+//   window.innerHeight || document.documentElement.clientHeight;
+
+const LS_SCORE_KEY = "score";
+
+const scoreArea = document.getElementById("score");
+const modal = document.getElementById("modal");
+const resetScoreButton = document.getElementById("reset-score-btn");
+const playAgainBtn = document.getElementById("play-again-btn");
+const cakeImage = document.querySelector(".target");
+
 const cakeWidth = 100;
 const cakeHeight = 100;
-const maxX = windowWidth - cakeWidth;
-const maxY = windowHeight - cakeHeight;
-const randomX = Math.floor(Math.random() * maxX);
-const randomY = Math.floor(Math.random() * maxY);
+// const maxX = windowWidth - cakeWidth;
+// const maxY = windowHeight - cakeHeight;
 
-const scoreArea = document.getElementById('score');
-let score = localStorage.getItem('score');
+function setRandomPositionToCake() {
+  const randomX = Math.floor(Math.random() * window.innerWidth - cakeWidth);
+  const randomY = Math.floor(Math.random() * window.innerHeight - cakeHeight);
+  cakeImage.style.top = randomY + "px";
+  cakeImage.style.left = randomX + "px";
+}
 
-cakeImage.style.top = randomY + 'px';
-cakeImage.style.left = randomX + 'px';
+function hideModal() {
+  modal.style.display = "none";
+}
 
-cakeImage.addEventListener('click', function() {
-const modal = document.getElementById('modal');
-modal.style.display = 'block';
-    score++; // Exercise 4
-    scoreArea.textContent = score;
-    localStorage.setItem('score', score); //Exercise 5
+function showModal() {
+  modal.style.display = "block";
+}
+
+setRandomPositionToCake();
+
+let score = parseInt(localStorage.getItem(LS_SCORE_KEY)) || 0;
+
+cakeImage.addEventListener("click", function () {
+  showModal();
+  score++; // Exercise 4
+  scoreArea.textContent = score;
+  localStorage.setItem(LS_SCORE_KEY, score); //Exercise 5
 });
 
-const playAgainBtn = document.getElementById('play-again-btn');
-playAgainBtn.addEventListener('click', function() {
-const modal = document.getElementById('modal');
-modal.style.display = 'none';
-
-const newRandomX = Math.floor(Math.random() * maxX);
-const newRandomY = Math.floor(Math.random() * maxY);
-cakeImage.style.top = newRandomY + 'px';
-cakeImage.style.left = newRandomX + 'px';
+playAgainBtn.addEventListener("click", function () {
+  hideModal();
+  setRandomPositionToCake();
 });
-
 
 // Exercise 5
 /*
@@ -42,9 +54,11 @@ cakeImage.style.left = newRandomX + 'px';
 */
 
 // Exercise 6
-const resetScoreButton = document.getElementById('reset-score-btn');
-resetScoreButton.addEventListener('click', function() {
-    score = 0;
-    scoreArea.textContent = score;
-    localStorage.setItem('score', score);
+resetScoreButton.addEventListener("click", function () {
+  score = 0;
+  scoreArea.textContent = score;
+  localStorage.removeItem(LS_SCORE_KEY);
+
+  hideModal();
+  setRandomPositionToCake();
 });
