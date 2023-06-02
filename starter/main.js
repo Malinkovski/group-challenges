d861f966ca9fb9179bcacf7c3687a3992145d68f
Ilija Malinkovski <ilija.malinkovski@gmail.com>
2023-06-02 17:57:16 +0200

    Exercise 4, 5 and 6 done.

--- starter/main.js
+++ starter/main.js
@@ -8,12 +8,18 @@ const maxY = windowHeight - cakeHeight;
 const randomX = Math.floor(Math.random() * maxX);
 const randomY = Math.floor(Math.random() * maxY);
 
+const scoreArea = document.getElementById('score');
+let score = localStorage.getItem('score');
+
 cakeImage.style.top = randomY + 'px';
 cakeImage.style.left = randomX + 'px';
 
 cakeImage.addEventListener('click', function() {
 const modal = document.getElementById('modal');
 modal.style.display = 'block';
+    score++; // Exercise 4
+    scoreArea.textContent = score;
+    localStorage.setItem('score', score); //Exercise 5
 });
 
 const playAgainBtn = document.getElementById('play-again-btn');
@@ -25,4 +31,20 @@ const newRandomX = Math.floor(Math.random() * maxX);
 const newRandomY = Math.floor(Math.random() * maxY);
 cakeImage.style.top = newRandomY + 'px';
 cakeImage.style.left = newRandomX + 'px';
+});
+
+
+// Exercise 5
+/*
+ window.addEventListener('beforeunload', function() {
+    localStorage.setItem('score', score);
+});
+*/
+
+// Exercise 6
+const resetScoreButton = document.getElementById('reset-score-btn');
+resetScoreButton.addEventListener('click', function() {
+    score = 0;
+    scoreArea.textContent = score;
+    localStorage.setItem('score', score);
 });
