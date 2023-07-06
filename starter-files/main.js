0767788cad3ea80ea9b883c008b1f2ece539c674
Ilija Malinkovski <ilija.malinkovski@gmail.com>
2023-07-06 23:48:52 +0200

    Exercise 3 and 4 done.

--- starter-files/main.js
+++ starter-files/main.js
@@ -26,29 +26,34 @@ class Guest {
 
 const guestList = [];
 
+function createTableRow(name, age) {
+  const newRow = $("<tr>");
+  const nameCell = $("<td>").text(name);
+  const ageCell = $("<td>").text(age);
+  newRow.append(nameCell, ageCell);
+  return newRow;
+}
+
 // Que function
 function addToQue() {
   const queInstance = new Que();
   queList.push(queInstance);
 
   const queTableBody = $("#queList tbody");
-  const newRow = $("<tr>");
-  const nameCell = $("<td>").text(queInstance.name);
-  const ageCell = $("<td>").text(queInstance.age);
+  const newRow = createTableRow(queInstance.name, queInstance.age);
   const actionsCell = $("<td>");
 
   const acceptButton = $("<button>")
-    .addClass("btn btn-sm btn-outline-success")
+    .addClass("btn btn-sm btn-outline-success mr-lg-1")
     .text("Accept")
     .hide();
 
   const declineButton = $("<button>")
-    .addClass("btn btn-sm btn-outline-danger mr-1")
+    .addClass("btn btn-sm btn-outline-danger")
     .text("Decline");
 
   actionsCell.append(acceptButton, declineButton);
-
-  newRow.append(nameCell, ageCell, actionsCell);
+  newRow.append(actionsCell);
   queTableBody.append(newRow);
 
   if (queInstance.isAdult()) {
@@ -60,3 +65,68 @@ $(document).ready(function () {
   $("#addToQue").click(addToQue);
 });
 
+//exercise III
+
+//AddToGuestList function
+function addToGuestList(name, age) {
+  const guestInstance = new Guest(name, age);
+  guestList.push(guestInstance);
+
+  if (guestList.length >= 15) {
+    alert("Sorry we are full, come back another night!");
+    return;
+  }
+
+  const guestTableBody = $("#guestList tbody");
+  const newRow = createTableRow(guestInstance.name, guestInstance.age);
+  const actionsCell = $("<td>");
+
+  const editButton = $("<button>")
+    .addClass("btn btn-sm btn-outline-info")
+    .text("Edit");
+
+  actionsCell.append(editButton);
+  newRow.append(actionsCell);
+  guestTableBody.append(newRow);
+}
+
+//Accept button
+$("#queList").on("click", ".btn-outline-success", function () {
+  const row = $(this).parents("tr");
+  const name = row.find("td:first-child").text();
+  const age = +row.find("td:nth-child(2)").text();
+  addToGuestList(name, age);
+  row.remove();
+});
+
+//Decline button
+$("#queList").on("click", ".btn-outline-danger", function () {
+  $(this).closest("tr").remove();
+});
+
+//Exercise IV/V
+
+//Edit/Save button
+
+function getName() {}
+
+$("#guestList").on("click", ".btn-outline-info", function () {
+  const row = $(this).closest("tr");
+  const nameCell = row.find("td:first-child");
+  const originalNameValue = nameCell.text();  
+
+  if ($(this).text() === "Edit") {
+    const nameInputForm = $("<input>")
+      .addClass("form-control-sm form-control")
+      .val(originalNameValue);
+
+    nameCell.text("").append(nameInputForm);
+    $(this).text("Save");
+  } else {
+    const nameInputFormValue = nameCell.find("input").val();
+    const indexOfRow = row.index();
+    guestList[indexOfRow].name = nameInputFormValue;
+    nameCell.text(nameInputFormValue);
+    $(this).text("Edit");
+  }
+});
